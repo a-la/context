8c22c2cc85ead65a5e530b60d1d1a7aea3416aba
Anton <anton@adc.sh>
2018-08-16 16:43:31 +0100

    implementation

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
 import { debuglog } from 'util'
+import { Replaceable } from 'restream'
+import Catchment from 'catchment'
 
 const LOG = debuglog('@a-la/context')
 
 /**
- * A test context for @a-la packages.
- * @param {Config} config Configuration object.
- * @param {string} config.type The type.
+ * A context for testing à la transforms which will allow to use its `stream` property.
  */
-export default async function context(config = {}) {
-  const {
-    type,
-  } = config
-  LOG('@a-la/context called with %s', type)
-  return type
+export default class ALaContext {
+  /**
+   * Create a Replaceable stream with a given rule or rules.
+   * @param {Rule|Rule[]} rules A rule or rules to use.
+   * @param {string} text The string to feed to the stream.
+   * @param {string[]} [eventKeys] Which events to listen to.
+   * @returns {ReturnType} Replaceable instance, string result and events map.
+   */
+  async stream(rules, text, eventKeys = []) {
+    if (!text) throw new Error('An input text is required.')
+
+    const replaceable = new Replaceable(rules)
+    const events = eventKeys.reduce((acc, key) => ({ ...acc, [key]: [] }), {})
+    eventKeys.forEach((key) => {
+      replaceable.on(key, (data) => {
+        events[key].push(data)
+      })
+    })
+    const { promise } = new Catchment({
+      rs: replaceable,
+    })
+    replaceable.end(text)
+    const result = await promise
+    return { events, result, replaceable }
+  }
 }
 
 /**
- * @typedef {Object} Config
- * @property {string} type The type.
+ * @typedef {import('restream').Rule} Rule
+ *
+ * @typedef {Object} ReturnType
+ * @prop {Object.<string, any[]>} events Emitted events recorded against passed events keys.
+ * @prop {string} result The caught output of the stream.
+ * @prop {Replaceable} replaceable The instance of the Replaceable stream.
  */
